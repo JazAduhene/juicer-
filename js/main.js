451f9594b4dbee7d4bdcc6bd1c034ac12df99f70
JazAduhene <jasmine.aduhene@hotmail.com>
2021-03-19 23:47:04 +0000

    Added mailto email link to website also added smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+// Navbar Menu Toggle
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+// Email Trigger
+function sendEmail(address) {
+  window.location.href = 'mailto:' + address;
+}
